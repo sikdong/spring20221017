94a35daaac3d1535ac1507e9d242fa891756a5dc
sikdong <zmstnv1119@naver.com>
2022-10-23 13:28:24 +0900

    1023 CRUD 게시판 연습

---
+++ src/main/java/org/zerock/controller/domain/lecture/Customer.java
@@ -0,0 +1,56 @@
+package org.zerock.controller.domain.lecture;
+import lombok.Data;
+
+@Data
+public class Customer {
+	private int id;
+	private String name;
+	private String contactName;
+	private String address;
+	private String city;
+	private String postalCode;
+	private String country;
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getContactName() {
+		return contactName;
+	}
+	public void setContactName(String contactName) {
+		this.contactName = contactName;
+	}
+	public String getAddress() {
+		return address;
+	}
+	public void setAddress(String address) {
+		this.address = address;
+	}
+	public String getCity() {
+		return city;
+	}
+	public void setCity(String city) {
+		this.city = city;
+	}
+	public String getPostalCode() {
+		return postalCode;
+	}
+	public void setPostalCode(String postalCode) {
+		this.postalCode = postalCode;
+	}
+	public String getCountry() {
+		return country;
+	}
+	public void setCountry(String country) {
+		this.country = country;
+	}
+	
+}
