19e8ff338248fe1fe85788c973a9c0c18d7844ea
sikdong <zmstnv1119@naver.com>
2022-10-26 17:40:17 +0900

    1026 mybatis 시작

---
+++ src/main/java/org/zerock/mapper/lecture/Mapper02.java
@@ -0,0 +1,14 @@
+package org.zerock.mapper.lecture;
+
+import java.time.LocalDate;
+import java.util.List;
+
+public interface Mapper02 {
+	List<String> getCustomerNames();
+	
+	List<Integer> getCustomerIds();
+	
+	List<Double> getProductPrices();
+	
+	List<LocalDate> getBirthDate();
+}
---
+++ src/main/java/org/zerock/mapper/lecture/Mapper05.java
@@ -0,0 +1,17 @@
+package org.zerock.mapper.lecture;
+
+
+import org.zerock.domain.lecture.JavaBean05;
+import org.zerock.domain.lecture.JavaBean08;
+import org.zerock.domain.lecture.JavaBean09;
+import org.zerock.domain.lecture.JavaBean10;
+
+public interface Mapper05 {
+	JavaBean05 getOrder();
+
+	JavaBean08 getOrder2();
+
+	JavaBean09 getProduct();
+	
+	JavaBean10 getSupplier();
+}
