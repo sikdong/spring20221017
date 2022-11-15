6444f014bb9b27129d5497e221991f74c3b42d6f
sikdong <zmstnv1119@naver.com>
2022-11-15 10:03:18 +0900

    검색 시 페이지네이션 오류 나는 거 해결

--- src/main/java/org/zerock/service/practice/CustomerService.java
+++ src/main/java/org/zerock/service/practice/CustomerService.java
@@ -21,9 +21,9 @@ public class CustomerService {
 		int start = (page - 1) * end;
 		
 		double leftPageNumber = Math.max(1, page-3);
-		double rightPageNumber = Math.min(9,leftPageNumber + 5);
 		int countAll = mapper.countAll(column, "%"+keyword+"%"); // SELECT Count(*) FROM Board
 		int lastPageNumber = (countAll - 1) / end + 1;
+		double rightPageNumber = Math.min(lastPageNumber,leftPageNumber + 5);
 		
 		pageButtonInfo.setLeftPageNumber(leftPageNumber);
 		pageButtonInfo.setRightPageNumber(rightPageNumber);
